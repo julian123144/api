4b62dc85fcd1f5881bad750e647425c6180bb685
julian123144 <56325866+julian123144@users.noreply.github.com>
2021-03-23 23:51:11 -0600

    Add files via upload

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\models\Movie;
+
+class MovieController extends Controller
+{
+    public function index(){
+        return Movie::all();
+    }
+
+    public function show($id){
+        return Movie::find($id);
+    }
+
+    public function store(Request $request){
+        $movie = Movie::create($request->all());
+        return response()->json($movie, 201);
+    }
+
+    public function update(Request $request, $id){
+        $movie = Movie::findOrFail($id);
+        $movie->update($request->all());
+        return response()->json($movie, 200);
+    }
+
+    public function delete($id){
+        $movie = Movie::findOrFail($id);
+        $movie->delete();
+        return response(null,204);
+    }
+
+
+}
---
+++ database/seeders/MovieTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+use App\models\Movie;
+
+class MovieTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Movie::truncate();
+        $faker = \Faker\Factory::create();
+        for($i = 0; $i < 10; $i++){
+            Movie::create([
+                'title' => $faker->sentence,
+                'synopsis' => $faker->paragraph,
+                'year' => $faker->randomDigit,
+                'cover' => $faker->sentence,
+            ]);
+
+            
+        }
+    }
+}
